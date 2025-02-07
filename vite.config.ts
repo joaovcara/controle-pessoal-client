0fa22708cbe2525bd9fdb3f6eeb991c1246a21c4
joao.cara <joao.cara@outlook.com.br>
2025-02-07 14:55:13 -0300

    Ajuste roteamento de imagens

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: './', // Base URL para o aplicativo
+export default defineConfig(() => {
+  return {
+    plugins: [react()],
+    base: '/controlepessoal/', // Define a base URL para produção e desenvolvimento
+  }
 })
